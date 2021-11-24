58a32485c9579e416f11f8d70c484bc80f293cb9
marcelo <marcelo.rocha@clouddog.com.br>
2021-11-24 12:53:04 -0300

    feat(): fix bug of nodeId

--- gatsby-node.js
+++ gatsby-node.js
@@ -43,27 +43,27 @@ async function InfoCloudOfertas (actions, createContentDigest, createNodeId, plu
 
     if (data.banners.length > 0) {
       data.banners.forEach(banner => {
-        createNodeHandler(createNode, createNodeId, createContentDigest, banner, 'CloudOfertasBanner')
+        createNodeHandler(createNode, createContentDigest, banner, 'CloudOfertasBanner')
       })
     }
     if (data.lojas.length > 0) {
       data.lojas.forEach(loja => {
-        createNodeHandler(createNode, createNodeId, createContentDigest, loja, 'CloudOfertasLoja')
+        createNodeHandler(createNode, createContentDigest, loja, 'CloudOfertasLoja')
       })
     }
     if (data.tabloides.length > 0) {
       data.tabloides.forEach(tabloide => {
-        createNodeHandler(createNode, createNodeId, createContentDigest, tabloide, 'CloudOfertasTabloide')
+        createNodeHandler(createNode, createContentDigest, tabloide, 'CloudOfertasTabloide')
       })
     }
     if (data.categories.length > 0) {
       data.categories.forEach(categoria => {
-        createNodeHandler(createNode, createNodeId, createContentDigest, categoria, 'CloudOfertasCategoria')
+        createNodeHandler(createNode, createContentDigest, categoria, 'CloudOfertasCategoria')
       })
     }
     if (data.forms.length > 0) {
       data.forms.forEach(form => {
-        createNodeHandler(createNode, createNodeId, createContentDigest, form, 'CloudOfertasForm')
+        createNodeHandler(createNode, createContentDigest, form, 'CloudOfertasForm')
       })
     }
   } catch (err) {
@@ -71,7 +71,7 @@ async function InfoCloudOfertas (actions, createContentDigest, createNodeId, plu
   }
 }
 
-function createNodeHandler (createNode, createNodeId, createContentDigest, item, type) {
+function createNodeHandler (createNode, createContentDigest, item, type) {
   createNode({
     ...item,
     parent: null,
