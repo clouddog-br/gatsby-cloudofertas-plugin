d9b7b73aa2b04a59e25a9efc2017a21efc7d613e
marcelo <marcelo.rocha@clouddog.com.br>
2021-11-25 11:27:25 -0300

    add placeholder in textarrea

--- Form/textarea.js
+++ Form/textarea.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TextArea = ({ field, register, errors }) => {
+const TextArea = ({ field, register, errors, placeholder }) => {
   return (
     <>
       <label htmlFor={field.name}>{field.label}</label>
