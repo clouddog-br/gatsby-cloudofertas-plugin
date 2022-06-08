87c0543ec0828fd255246b509637a9d2a7295174
Marcelo <marcelor068@gmail.com>
2022-06-08 11:23:59 -0300

    feat(): update checkbox

--- Form/checkbox.js
+++ Form/checkbox.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import parse from 'html-react-parser'
+
 const Checkbox = ({ field, register, errors, errorLabel }) => {
   return (
     <>
-   {errorLabel === 'top' && <small>{errors[field.name] && 'Campo obrigatatório'}</small>}
-    <div>
-      <input
-        id={field.name}
-        name={field.name}
-        type="checkbox"
-        {...register(field.name, { required: field.required })}
+      {errorLabel === 'top' && <small>{errors[field.name] && 'Campo obrigatatório'}</small>}
+      <div>
+        <input
+          id={field.name}
+          name={field.name}
+          type="checkbox"
+          {...register(field.name, { required: field.required })}
         />
-        <label htmlFor={field.name} className={`${errors[field.name] ? 'error' : ''}`}>{field.label}</label>
-    </div>
+        <label htmlFor={field.name} className={`${errors[field.name] ? 'error' : ''}`}>{parse(field.label)}</label>
+      </div>
     </>
   )
 }
